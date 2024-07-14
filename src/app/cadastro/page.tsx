d89314a2554b3eec30418277e0ac5a8a452a7e91
henriqueizar <henriqueizarmachado@gmail.com>
2024-07-14 12:39:51 -0300

    page

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,41 +1,51 @@
 "use client";
 
-import React from 'react';
-import '~/styles/cadastro.css';
+import React from "react";
+import "~/styles/cadastro.css";
 
 interface CadastroProps {
-    goToLogin: () => void;
+  goToLogin: () => void;
 }
 
 const Cadastro: React.FC<CadastroProps> = ({ goToLogin }) => {
-    return (
-        <div className="page-container">
-            <div className="signup-container">
-                <h2>Cadastro</h2>
-                <form>
-                    <div className="input-group">
-                        <label htmlFor="username">Usuário</label>
-                        <input type="text" id="username" name="username" required />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="password">Senha</label>
-                        <input type="password" id="password" name="password" required />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="confirm-password">Confirmar Senha</label>
-                        <input type="password" id="confirm-password" name="confirm-password" required />
-                    </div>
-                    <button type="submit">Cadastrar</button>
-                </form>
-                <div className="login-link">
-                    <p>Já tem uma conta? <a href="/login" onClick={goToLogin}>Faça login</a></p>
-                </div>
-            </div>
-            <div className="home-link">
-                <a href="/">Voltar à página inicial</a>
-            </div>
+  return (
+    <div className="page-container">
+      <div className="signup-container">
+        <h2>Cadastro</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="username">Usuário</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Senha</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="confirm-password">Confirmar Senha</label>
+            <input
+              type="password"
+              id="confirm-password"
+              name="confirm-password"
+              required
+            />
+          </div>
+          <button type="submit">Cadastrar</button>
+        </form>
+        <div className="login-link">
+          <p>
+            Já tem uma conta?{" "}
+            <a href="/login" onClick={goToLogin}>
+              Faça login
+            </a>
+          </p>
         </div>
-    );
+      </div>
+      <div className="home-link">
+        <a href="/">Voltar à página inicial</a>
+      </div>
+    </div>
+  );
 };
 
 export default Cadastro;
